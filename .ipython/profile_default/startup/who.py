4d6649b89afab81a00a13673380601a72f3de3ae
Tom Boyd <tboyd@scitec.com>
2018-10-26 16:17:10 -0400

    ADD who ipython startup script

---
+++ .ipython/profile_default/startup/who.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def human_readable_size(size, fmt):
+    for unit in ['','kB','MB','GB','TB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:{fmt}} {unit}"
+
+def _who(vardict, sort=False):
+
+    d_realnames = {
+        x : y for x, y in vardict.items() if isinstance(y, np.ndarray)
+    }
+
+    namelen = max(max(map(len, d_realnames.keys())), 4)
+    shapelen = max(map(lambda x: len(str(x.shape)), d_realnames.values()))
+    total_len = 55 + namelen + shapelen
+
+    print(
+        f"{'dtype':<7} {'name':{namelen}} {'shape':{shapelen}} "
+        f"{'flags':<7} {'data_pointer':<14} {'nbytes':12} human_nbytes\n",
+        "=" * total_len, sep=""
+    )
+
+    it = sorted(d_realnames.items()) if sort else d_realnames.items()
+    for x, y in it:
+        flags = ''.join([
+            "C" if y.flags.c_contiguous else "",
+            "F" if y.flags.f_contiguous else "",
+            "O" if y.flags.owndata else "",
+            "W" if y.flags.writeable else "",
+            "A" if y.flags.aligned else "",
+            "X" if y.flags.writebackifcopy else ""])
+        print(
+            f"{y.dtype.str:7} {x:{namelen}} {str(y.shape):{shapelen}} "
+            f"{flags:7} 0X{y.ctypes._as_parameter_.value:12X} {y.nbytes:<12}",
+            human_readable_size(y.nbytes, "<6.2f")
+        )
+
+get_ipython().define_macro('woo','_who(globals())')
