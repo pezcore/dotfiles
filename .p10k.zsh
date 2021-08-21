fe393a926484b36cf19e7cf194c663abf862fbac
Tom Boyd <tvboyd23@gmail.com>
2021-08-21 14:36:57 -0400

    UPD: p10k clock color

--- .p10k.zsh
+++ .p10k.zsh
@@ -1547,7 +1547,7 @@
 
   ####################################[ time: current time ]####################################
   # Current time color.
-  typeset -g POWERLEVEL9K_TIME_FOREGROUND=16
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=157
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   # If set to true, time will update when you hit enter. This way prompts for the past
